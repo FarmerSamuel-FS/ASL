03b54011b4319832fed689f7166d764ec7982a8c
Samuel Farmer <141048125+FarmerSamuel-FS@users.noreply.github.com>
2024-05-15 19:09:55 -0400

    Delete Java directory

--- Java/Hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello ASL!");
-        System.out.println("Today is " + new Date());
-    }
-}
-
